32c277eaf1ee03e79d4f6397389524704e620530
devShamim <shamimeforb@gmail.com>
2022-04-05 23:51:40 +0600

    fixed inquiry form issue

--- inquiry.php
+++ inquiry.php
@@ -68,7 +68,7 @@ if($_POST) {
     $headers  = 'MIME-Version: 1.0' . "\r\n"
     .'Content-type: text/html; charset=utf-8' . "\r\n"
     .'From: ' . $visitor_email . "\r\n";
-    if(mail($recipient, $user_name, $visitor_email, $message, $email_body, $headers)) {
+    if(mail($recipient, $visitor_email, $email_body, $headers)) {
         echo "<p>Thank you for contacting us, $user_name. You will get a reply within 24 hours.</p>";
     } else {
         echo '<p>We are sorry but the email did not go through.</p>';
