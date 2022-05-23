b38a23a74bbf47c74673b34c66bb238c382ce9ef
devShamim <shamimeforb@gmail.com>
2022-05-23 22:14:56 +0600

    commit changes

--- send.php
+++ send.php
@@ -21,7 +21,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "mail@yourdomain.com";
+        $recipient = "contact@yacht-and-go.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
